5fb116d8d02e61ec2b865258befe75565775163e
mzjmc123 <mzjmc123@163.com>
2018-03-10 17:57:06 +0800

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 555
                 </div>
 
                 <div class="links">
